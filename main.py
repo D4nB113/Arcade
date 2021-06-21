6755b31ecf2db492f4ec86f253115d05909498d8
Daniel Bentley <danielbentley@acsstainless.co.uk>
2021-06-21 16:08:26 +0100

    Basic layout

--- main.py
+++ main.py
@@ -1,2 +1,87 @@
+import arcade
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.coin_list = None
+        self.wall_list = None
+        self.player_list = None
+
+        self.player_sprite = None
+
+        self.physics_engine = None
+
+        arcade.set_background_color(arcade.csscolor.BLACK)
+
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512,96], [256,96], [768,96]]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = -0
+
+    def on_update(self, delta_time: float):
+        self.physics_engine.update()
+
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
 if __name__ == '__main__':
-    print("IT WORKS!")
+    main()
